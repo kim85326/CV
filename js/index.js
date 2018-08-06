d834a00c4e2ac4560c23368f80b75292c298eedc
kim85326 <kim85326@gmail.com>
2018-08-06 21:56:58 +0800

    第一版完成

--- js/index.js
+++ js/index.js
@@ -1,5 +1,44 @@
 $(document).ready(function(){
 
+	// skill chart
+	var ctx = $("#myChart");
+	var myChart = new Chart(ctx, {
+		type: 'bar',
+		data: {
+			labels: ['HTML/CSS', 'Sass/Scss', 'Bootstrap', 'JS/jQuery', 'Vue', 'PHP', 'MySQL', 'Nodejs'],
+			datasets: [{
+				label: '',
+				data: [5, 3, 5, 4, 3, 4, 5, 3],
+				backgroundColor: [ // 背景色
+					"#e881a7",
+					"#e881a7",
+					"#e881a7",
+					"#e881a7",
+					"#e881a7",
+					"#e881a7",
+					"#e881a7",
+					"#e881a7"
+				],
+			}]
+		},
+		options: {
+			scales: {
+	            yAxes: [{
+	                ticks: {
+	                    max: 5,
+                		min: 0,
+                		stepSize: 1
+	                }
+	            }]
+	        },
+	        animation: {
+	           duration: 3000,
+	           easing: 'easeOutQuart'
+        	}
+	    }
+	});
+
+
 	// header 改變顏色
 	$(window).on("scroll",function(event) {
 		var scroll_distance = $(window).scrollTop();
